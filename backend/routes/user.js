97a7e35702f09d9b5e58dd46c82509dd74bc571a
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-09-29 20:06:21 +0530

    added an endpoint for login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,6 +5,7 @@ const app = express()
 const User = require('../models/User')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
 
 router.post('/createuser', [
     body("password").isLength({ min: 5 }).withMessage("Enter a password of min length of 5 numbers"),
@@ -26,7 +27,13 @@ router.post('/createuser', [
                 email: req.body.email,
                 password: hash
             })
-            res.send("Sucessfully created")
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authtoken = jwt.sign(data, process.env.VITE_JWT_SECRET);
+            res.send({ "success": "Successfully created", authtoken })
         }
     } catch (error) {
         res.send(error.message)
@@ -37,7 +44,28 @@ router.post('/login', [
     body("password").isLength({ min: 5 }).withMessage("Enter a password of min length of 5 numbers"),
     body("email").isEmail().withMessage("Please enter a valid email")
 ], async (req, res) => {
-    
+    try {
+        // CHECKING IF THE USER EXISTS WITH THE GIVEN EMAIL
+        const user = await User.findOne({ "email": req.body.email })
+
+        // IF DOES NOT EXIST
+        if (!user) {
+            res.send("Incorrect email or password")
+        }
+
+        // IF EXIST THEN CHECKING THE PASSWORD
+        const auth = await bcrypt.compare(req.body.password, user.password)
+        if (!auth) {
+            res.send("Incorrect email or password")
+        }
+
+        // LOGGED IN
+        res.status(201).json({ message: "User logged in successfully", success: true });
+    } catch (error) {
+        res.json(error.message);
+    }
+
+
 })
 
 module.exports = router
