a17e1f7afa68b38dbccc28e02fb4b93f63c6edba
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-10-08 20:13:02 +0530

    added a navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Box, Grid, Typography, createTheme } from '@mui/material';
 import './App.css'
+import Button from '@mui/material/Button';
+import { lightBlue, lightGreen } from '@mui/material/colors';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className=''>
+      <Grid sx={{p: 2}}
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="flex-start">
+        <Typography variant="h3" component="h4" color={lightBlue[800]}>
+          Blog.Dev
+        </Typography>
+        <Box sx={{width: 250, display: 'flex', justifyContent: 'space-around'}}>
+          <Button variant="contained" size='medium'>Sing In</Button>
+          <Button variant="contained" size='medium'>Log In</Button>
+        </Box>
+      </Grid>
+    </div>
   )
 }
 
