7215263c057b1b4277a52652343983d9920f7d96
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-11-27 23:49:27 +0530

    added dubby blogs

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import Blog_card from './Blog_card'
 
 const Home = () => {
+    let title = "Bitcoin: Decentralized Digital Currency and the Future of Peer-to-Peer Transactions"
+    let body = "Decentralized digital currency, operates on blockchain, limited supply, peer-to-peer transactions, volatile value, potential store of value or payment method."
     return (
         <div>
             <nav className='flex px-10 py-4 justify-between'>
@@ -12,8 +14,14 @@ const Home = () => {
                     <Link to={'/profile'} className='bg-[#f14843] w-20 rounded-xl font-medium text-center py-2 my-auto'>Profile</Link>
                 </div>
             </nav>
-            <div className='m-8'>
-                <Blog_card />
+            <div className='m-8 flex flex-col items-center'>
+                <Blog_card title = {title} body = {body} img_url = "https://th.bing.com/th?id=OSK.HEROu9H3ZxVAq44jb9Jv0eitJt7Rk8ArgSbDL6b1zkZ3XfI&w=384&h=228&c=1&rs=2&o=6&dpr=1.4&pid=SANGAM"/>
+                <Blog_card title = {title} body = {body} img_url = "https://th.bing.com/th?id=OSK.HEROu9H3ZxVAq44jb9Jv0eitJt7Rk8ArgSbDL6b1zkZ3XfI&w=384&h=228&c=1&rs=2&o=6&dpr=1.4&pid=SANGAM"/>
+                <Blog_card title = {title} body = {body} img_url = "https://th.bing.com/th?id=OSK.HEROu9H3ZxVAq44jb9Jv0eitJt7Rk8ArgSbDL6b1zkZ3XfI&w=384&h=228&c=1&rs=2&o=6&dpr=1.4&pid=SANGAM"/>
+                <Blog_card title = {title} body = {body} img_url = "https://th.bing.com/th?id=OSK.HEROu9H3ZxVAq44jb9Jv0eitJt7Rk8ArgSbDL6b1zkZ3XfI&w=384&h=228&c=1&rs=2&o=6&dpr=1.4&pid=SANGAM"/>
+                <Blog_card title = {title} body = {body} img_url = "https://th.bing.com/th?id=OSK.HEROu9H3ZxVAq44jb9Jv0eitJt7Rk8ArgSbDL6b1zkZ3XfI&w=384&h=228&c=1&rs=2&o=6&dpr=1.4&pid=SANGAM"/>
+                <Blog_card title = {title} body = {body} img_url = "https://th.bing.com/th?id=OSK.HEROu9H3ZxVAq44jb9Jv0eitJt7Rk8ArgSbDL6b1zkZ3XfI&w=384&h=228&c=1&rs=2&o=6&dpr=1.4&pid=SANGAM"/>
+                
             </div>
         </div>
     )
